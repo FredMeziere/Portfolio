fb515281f5dd486c102d977612f80f2c66885fe0
Fred Meziere <Fredzem@outlook.fr>
2023-01-31 14:14:15 +0100

    Ajout des différentes routes au router

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -2,6 +2,19 @@ const mainController = {
     home: (req, res) => {
       res.render('main/accueil');
     },
+   
+    about: (req, res) => {
+      res.render('main/apropos');
+    },
+
+    realisations: (req, res) => {
+      res.render('main/relisations');
+    },
+
+    contact: (req, res) => {
+      res.render('main/contact');
+    },
+
     notFound: (req, res) => {
       res.status(404).render('404/404');
     },
--- app/router.js
+++ app/router.js
@@ -4,4 +4,12 @@ const mainController = require ('./controllers/mainController');
 
 router.get('/', mainController.home);
 
+router.get('/apropos', mainController.about);
+
+router.get('/realisations', mainController.home);
+
+router.get('/contact', mainController.about);
+
+router.use(mainController.notFound);
+
 module.exports = router;

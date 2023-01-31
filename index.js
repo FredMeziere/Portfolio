8dd51b858399a4ee98045df07938bc3c8b50aaa1
Fred Meziere <Fredzem@outlook.fr>
2023-01-31 17:50:42 +0100

    Ajout d'un fichier css pour le 404 modification de la vue 404 modifications diverses de html/css

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 
 app.use(router);
 
